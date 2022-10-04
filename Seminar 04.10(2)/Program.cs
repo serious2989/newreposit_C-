8de6890886d3dd3390327ed9553543f5b6345bab
Dmitriy Vasilyev <serious29042008@yandex.ru>
2022-10-04 10:35:49 +0300

    new commit

--- Seminar 04.10(2)/Program.cs
+++ Seminar 04.10(2)/Program.cs
@@ -5,4 +5,5 @@ int[] array = new int[8];
 for (int i = 0; i < array.Length; i++)
     array[i] = new Random().Next(-10, 11);
 
-Console.WriteLine("[" + string.Join(", ", array) + "]");
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
