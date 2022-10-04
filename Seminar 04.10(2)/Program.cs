a423ab4b20dff574ade61e8da68002f24f25fc19
Dmitriy Vasilyev <serious29042008@yandex.ru>
2022-10-04 11:03:49 +0300

    задача 2(1)

--- Seminar 04.10(2)/Program.cs
+++ Seminar 04.10(2)/Program.cs
@@ -1,9 +1,4 @@
 ﻿Console.Clear();
 int n = new Random().Next(100, 1000);
-Console.WriteLine(n);
-int n1 = n / 100;
-
-int n2 = n % 10;
-
-
-Console.WriteLine(n1 * 10 + n2);
+Console.WriteLine($"{n}\n{n / 100 * 10 + n % 10}");
+// сложное решение
