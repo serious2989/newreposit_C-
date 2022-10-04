c97f4aafa17c439a1b15d122f2934531f82a6942
Dmitriy Vasilyev <serious29042008@yandex.ru>
2022-10-04 11:15:10 +0300

    задача 3

--- Seminar 04.10(2)/Program.cs
+++ Seminar 04.10(2)/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.Clear();
-int n = new Random().Next(100, 1000);
-Console.WriteLine($"{n}\n{n / 100 * 10 + n % 10}");
-// сложное решение
+﻿//12. //Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деление.
+//34, 5 -> не кратно, остаток 4 
+//16, 4 -> кратно
+Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+if (n % m == 0)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine($"no, ostatok {n % m}");
