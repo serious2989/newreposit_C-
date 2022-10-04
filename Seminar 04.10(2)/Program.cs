0dce94e17d72eef56f2fe859e64548f7e7e0dc14
Dmitriy Vasilyev <serious29042008@yandex.ru>
2022-10-04 10:34:09 +0300

    zadacha

---
+++ Seminar 04.10(2)/Program.cs
@@ -0,0 +1,8 @@
+﻿// array
+
+int[] array = new int[8];
+// int n = 10;
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(-10, 11);
+
+Console.WriteLine("[" + string.Join(", ", array) + "]");
