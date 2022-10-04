7b5b116c424c85ceefd7222c08e0f1a60a001dbe
Dmitriy Vasilyev <serious29042008@yandex.ru>
2022-10-04 10:48:37 +0300

    коменты ставить так

--- Seminar 04.10(2)/Program.cs
+++ Seminar 04.10(2)/Program.cs
@@ -1,12 +1,13 @@
-﻿// Функции, процедура
-
+﻿/*
 Console.Clear();
-string f(int a, int b) // функция
+string f(int a, int b)
 {
     if (a > b)
         return ">";
-        
+
     return "<";
 }
 
-Console.WriteLine(f(7, 5));
+Console.WriteLine(f(5, 7));
+так ставятся коменты
+*/ 
