d6577feb60841bb3e8247e78eb55518bc4b764cc
Dmitriy Vasilyev <serious29042008@yandex.ru>
2022-10-04 11:24:56 +0300

    задача 4

--- Seminar 04.10(2)/Program.cs
+++ Seminar 04.10(2)/Program.cs
@@ -1,10 +1,6 @@
-﻿//12. //Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деление.
-//34, 5 -> не кратно, остаток 4 
-//16, 4 -> кратно
-Console.Clear();
+﻿Console.Clear();
 int n = Convert.ToInt32(Console.ReadLine());
-int m = Convert.ToInt32(Console.ReadLine());
-if (n % m == 0)
+if (n % 7 == 0 && n % 23 == 0)
     Console.WriteLine("yes");
 else
-    Console.WriteLine($"no, ostatok {n % m}");
+    Console.WriteLine("no");
