fd47ab6cabf18c4f872f5487ca18ec0edd4aff3e
Dmitriy Vasilyev <serious29042008@yandex.ru>
2022-10-04 11:25:30 +0300

    задача 4(1)

--- Seminar 04.10(2)/Program.cs
+++ Seminar 04.10(2)/Program.cs
@@ -1,4 +1,8 @@
-﻿Console.Clear();
+﻿//Напишите программу, которая принимает на вход 
+//число и проверяет, кратно ли оно одновременно 7 и 23.
+
+
+Console.Clear();
 int n = Convert.ToInt32(Console.ReadLine());
 if (n % 7 == 0 && n % 23 == 0)
     Console.WriteLine("yes");
