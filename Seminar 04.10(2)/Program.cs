274846d880ef8644ecacc511e97a594dbeac1de5
Dmitriy Vasilyev <serious29042008@yandex.ru>
2022-10-04 10:39:48 +0300

    new commit1

--- Seminar 04.10(2)/Program.cs
+++ Seminar 04.10(2)/Program.cs
@@ -1,9 +1,15 @@
-﻿// array
+﻿// Функции, процедура
 
-int[] array = new int[8];
-// int n = 10;
-for (int i = 0; i < array.Length; i++)
-    array[i] = new Random().Next(-10, 11);
+Console.Clear();
+void f(ref int x) // процедура
+{
+    x = x + 10;
+}
 
-Console.WriteLine($"[{string.Join(", ", array)}]");
 
+int n = 5;
+Console.WriteLine(n);
+f(ref n);
+Console.WriteLine(n);
+// сколько значений принимаем то столько и передаем
+// сколько значений передаем столько и принимаем
