35a90fd9bdccd2e993b24b294339fda27f709c05
Dmitriy Vasilyev <serious29042008@yandex.ru>
2022-10-04 11:31:18 +0300

    задача 5)

--- Seminar 04.10(2)/Program.cs
+++ Seminar 04.10(2)/Program.cs
@@ -1,10 +1,7 @@
-﻿//Напишите программу, которая принимает на вход 
-//число и проверяет, кратно ли оно одновременно 7 и 23.
-
-
-Console.Clear();
+﻿Console.Clear();
 int n = Convert.ToInt32(Console.ReadLine());
-if (n % 7 == 0 && n % 23 == 0)
+int m = Convert.ToInt32(Console.ReadLine());
+if (n * n == m || m * m == n)
     Console.WriteLine("yes");
 else
     Console.WriteLine("no");
