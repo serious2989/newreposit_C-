fb0fe75f2b81e1339a1ef7842e0cc11bd68e6b67
Dmitriy Vasilyev <serious29042008@yandex.ru>
2022-10-05 11:48:26 +0300

    Задача с днями недели

---
+++ Homeworks/homework 2(05.10)/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число дня недели");
+
+int day;
+
+day = Convert.ToInt32(Console.ReadLine());
+
+if(day <= 5)
+
+{
+
+Console.WriteLine("рабочий");
+
+}
+
+else
+{
+
+Console.WriteLine("выходной");
+
+}
+//не смог поставить, чтобы else заканчивал цикл на цифре 7. 
