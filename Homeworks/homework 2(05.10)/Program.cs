687a3fe82c6028b576a1aed072a16dc6a93a566b
Dmitriy Vasilyev <serious29042008@yandex.ru>
2022-10-05 12:15:14 +0300

    Поиск 2 числа из 3знач числа

--- Homeworks/homework 2(05.10)/Program.cs
+++ Homeworks/homework 2(05.10)/Program.cs
@@ -1,23 +1,28 @@
-﻿Console.Clear();
+﻿// Задача 10. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// (+проверка является ли число трехзначным)
 
-Console.WriteLine("Введите число дня недели");
-
-int day;
-
-day = Convert.ToInt32(Console.ReadLine());
-
-if(day <= 5)
-
-{
-
-Console.WriteLine("рабочий");
-
-}
-
+Console.Write("Напишите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int numberthree=DigitNumber(num);// Проверка трехзначного числа
+if (numberthree ==3)
+   {
+    int result = num / 10 % 10; // Находим вторую цифру числа
+    Console.WriteLine("Второе число "+ num + " - " + result);
+   }
 else
+   {
+    Console.WriteLine("Внимание "+ num + " это не трехзначное число" );
+   }
+
+
+int DigitNumber (int num)// Function to find number of digit
 {
-
-Console.WriteLine("выходной");
-
+int numberthree=0;
+double count = num;
+while (count>=1)
+   {
+    count=count/10;
+    numberthree++;
+   }
+return numberthree;
 }
-//не смог поставить, чтобы else заканчивал цикл на цифре 7. 
