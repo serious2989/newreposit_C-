7884955960aff004ec65bae65e05fa209f47db03
Dmitriy Vasilyev <serious29042008@yandex.ru>
2022-10-07 12:07:47 +0300

    Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве

--- Homeworks/Homework 3/Program.cs
+++ Homeworks/Homework 3/Program.cs
@@ -1,15 +1,15 @@
-﻿int number = ReadInt("Введите число N: ");
-
-for (int i = 1; i <= number; i++)
-{ 
-    Console.Write($"{i*i*i} ");
-}
-
-
-
-// Функция ввода сообщения
-int ReadInt(string message)
-{
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
-}
+﻿Console.Clear();
+Console.Write("Введите координату х1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату у1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату х2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату у2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z1: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z2: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(Math.Round(Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (x2 - z2) * (x2 - z2) ), 3));
+// решил так же как и на семинаре с добавлением 3 переменной. 
